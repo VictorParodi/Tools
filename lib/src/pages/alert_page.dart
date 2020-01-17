e3a204662bbba21a883707fd945df2dcb24d8861
Victor Parodi <victorparodi@Victors-MacBook-Pro.local>
2020-01-16 20:24:27 -0500

    [app1] commit 12: using the AlertDialog widget

--- lib/src/pages/alert_page.dart
+++ lib/src/pages/alert_page.dart
@@ -6,11 +6,45 @@ class AlertPage extends StatelessWidget {
     return Container(
       child: Scaffold(
         appBar: AppBar(title: Text('Alert Page')),
+        body: Center(
+          child: RaisedButton(
+            color: Colors.blue,
+            textColor: Colors.white,
+            child: Text('Show modal'),
+            onPressed: () {
+              _showModal(context);
+            },
+          ),
+        ),
         floatingActionButton: FloatingActionButton(
           onPressed: () => Navigator.pop(context),
           child: Icon(Icons.chevron_left),
         ),
       ),
+    );
+  }
+
+  void _showModal(context) {
+    showDialog(
+      context: context,
+      child: AlertDialog(
+        title: Text('Modal'),
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Text('Content for this modal element'),
+            FlutterLogo(size: 100.0,)
+          ],
+        ),
+        actions: <Widget>[
+          FlatButton(
+            child: Text('CLOSE'),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+          )
+        ],
+      )
     );
   }
 }
