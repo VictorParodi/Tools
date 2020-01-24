c2b95e6ef25148e3aee2034a75f978c6b7ea54e6
victor.parodi <victor.parodi@globant.com>
2020-01-23 22:33:56 -0500

    [app1] commit 13: playing around with avatars

--- lib/src/pages/avatar_page.dart
+++ lib/src/pages/avatar_page.dart
@@ -6,7 +6,41 @@ class AvatarPage extends StatelessWidget {
     return Container(
       child: Scaffold(
         appBar: AppBar(title: Text('Avatar Page')),
+        body: _createList(),
       ),
     );
+  }
+
+  Widget _createList() {
+    return ListView(
+      children: _createItems(),
+    );
+  }
+
+  List<Widget> _createItems() {
+    return [
+      Column(
+        children: <Widget>[
+          ListTile(
+            leading: CircleAvatar(child: Text('TO'),),
+            title: Text('Tile One'),
+            subtitle: Text('Tile subtitle'),
+          ),
+          Divider()
+        ],
+      ),
+
+      Column(
+        children: <Widget>[
+          ListTile(
+            leading: CircleAvatar(
+              backgroundImage: NetworkImage('https://qph.fs.quoracdn.net/main-qimg-70af0060bef82cc31c6e8e0035ddd144'),
+              ),
+            title: Text('Tile Two'),
+            subtitle: Text('Tile subtitle'),
+          )
+        ],
+      )
+    ];
   }
 }
