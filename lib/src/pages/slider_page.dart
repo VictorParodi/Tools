91b048545652aa8da35cda1ac262e8fcacdfea8e
Victor Parodi <victorparodi@Victors-MacBook-Pro.local>
2020-01-26 00:17:08 -0500

    [app1] commit 18: playing around with Switch widget

--- lib/src/pages/slider_page.dart
+++ lib/src/pages/slider_page.dart
@@ -7,12 +7,21 @@ class SliderPage extends StatefulWidget {
 
 class _SliderPageState extends State<SliderPage> {
   double _slidervalue = 20.0;
+  bool _switchvalue = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Slider Page')),
-      body: _createSlider()
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            _createSlider(),
+            _createSwitch()
+          ],
+        ),
+      )
     );
   }
 
@@ -24,8 +33,20 @@ class _SliderPageState extends State<SliderPage> {
         divisions: 50,
         min: 0.0,
         max: 500.0,
-        onChanged: (value) {
+        onChanged: _switchvalue? null : (value) {
           setState(() => _slidervalue = value);
+        },
+      )
+    );
+  }
+
+  Widget _createSwitch() {
+    return (
+      SwitchListTile(
+        title: Text('Slider blocker'),
+        value: _switchvalue,
+        onChanged: (value) {
+          setState(() => _switchvalue = value);
         },
       )
     );
